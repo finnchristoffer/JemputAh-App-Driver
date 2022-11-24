5a6dfa79f2c1165afbecf473e17e2110b0bc94fe
ThaddeusEvan <98073989+ThaddeusEvan@users.noreply.github.com>
2022-11-24 19:48:22 +0700

    Setup Assets and Reusable Component

---
+++ lib/constants/color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color brokenWhite = const Color.fromRGBO(245, 246, 233, 0.75);
+  static Color white = const Color.fromRGBO(255, 255, 255, 1.0);
+  static Color mainGreen = const Color.fromRGBO(85, 91, 77, 1.0);
+  static Color grayDecrement = const Color.fromRGBO(0, 0, 0, 0.2);
+  static Color black = const Color.fromRGBO(0, 0, 0, 1);
+  static Color backgroundGreen = const Color.fromRGBO(245, 246, 233, 1);
+  static Color buttonBackground = const Color.fromRGBO(62, 75, 42, 1);
+  static Color jadwalCardBackground = const Color.fromRGBO(178, 186, 159, 0.5);
+  static Color contactUs = const Color.fromRGBO(0, 0, 0, 0.8);
+  static Color secondaryBorder = const Color.fromRGBO(178, 186, 159, 1);
+  static Color separatorLine = const Color.fromRGBO(217, 217, 217, 1);
+  static Color hintTextColor = const Color.fromRGBO(0, 0, 0, 0.5);
+}
---
+++ lib/constants/image.dart
@@ -0,0 +1,8 @@
+const _pathImage = 'assets/images';
+
+const homeLogo = '$_pathImage/logo_panjang.png';
+const logo = '$_pathImage/logo.png';
+const carousel1 = '$_pathImage/carousel1.png';
+const carousel2 = '$_pathImage/carousel2.png';
+const carousel3 = '$_pathImage/carousel3.png';
+const profilePicture = '$_pathImage/profile_picture.jpg';
