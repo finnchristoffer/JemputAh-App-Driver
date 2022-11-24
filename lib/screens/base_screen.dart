398b0a433f9f8ace101a668cea184102dd58246b
ThaddeusEvan <98073989+ThaddeusEvan@users.noreply.github.com>
2022-11-24 20:27:14 +0700

    Base Screen and Featured Screen

---
+++ lib/screens/base_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:jemputah_app_driver/constants/color.dart';
+import 'package:jemputah_app_driver/constants/icons.dart';
+import 'package:jemputah_app_driver/constants/size.dart';
+import 'package:jemputah_app_driver/screens/featured_screen.dart';
+
+class BaseScreen extends StatefulWidget {
+  const BaseScreen({Key? key}) : super(key: key);
+
+  @override
+  _BaseScreenState createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    FeaturedScreen(),
+    FeaturedScreen(),
+    FeaturedScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: AppColors.white,
+          unselectedItemColor: AppColors.brokenWhite,
+          backgroundColor: AppColors.mainGreen,
+          elevation: 0,
+          items: [
+            BottomNavigationBarItem(
+              activeIcon: Image.asset(
+                isHome,
+                height: kBottomNavigationBarItemSize,
+              ),
+              icon: Image.asset(
+                isHomeNotSelected,
+                height: kBottomNavigationBarItemSize,
+              ),
+              label: "HOME",
+            ),
+            BottomNavigationBarItem(
+              activeIcon: Image.asset(
+                isPesanan,
+                height: kBottomNavigationBarItemSize,
+              ),
+              icon: Image.asset(
+                isPesananNotSelected,
+                height: kBottomNavigationBarItemSize,
+              ),
+              label: "PESANAN",
+            ),
+            BottomNavigationBarItem(
+              activeIcon: Image.asset(
+                isProfil,
+                height: kBottomNavigationBarItemSize,
+              ),
+              icon: Image.asset(
+                isProfilNotSelected,
+                height: kBottomNavigationBarItemSize,
+              ),
+              label: "PROFIL",
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          }),
+    );
+  }
+}
