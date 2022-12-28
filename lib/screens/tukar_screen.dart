b071f003272fa993e5dcc4bdae1fc29b059c9702
finnchristoffer <finn.christoffer@gmail.com>
2022-12-28 20:23:28 +0700

    add automaticallyImplyLeading: false

--- lib/screens/tukar_screen.dart
+++ lib/screens/tukar_screen.dart
@@ -35,6 +35,7 @@ class _TukarScreenState extends State<TukarScreen> {
     TextEditingController valuePoint = TextEditingController();
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: AppColors.mainGreen,
         title: const Text('Tukar Koin'),
         centerTitle: false,
